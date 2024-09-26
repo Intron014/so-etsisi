715ef4fb2a0f4f0dd15b366bcf7994a48a106454
Jorge Benjumea <me@intron014.com>
2024-09-26 18:23:44 +0200

    Ficheros p2.1 (The final commit)

--- trabajo/prc2.1/reventador.c
+++ trabajo/prc2.1/reventador.c
@@ -1,10 +1,13 @@
 #define _MINIX
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "generador.h"
+#include <string.h>
 
 /**
  * Muestra todas las palabras que puede genera el generador.
@@ -29,53 +32,108 @@ static int escribir_palabras(void)
 	return OK;
 }
 
-static int escribir_numword(const int ppg, const int num_procesos){
-	int word;
-	int proc;
-	int tot_pal;
-	tot_pal=total_palabras();
-	fprintf(stdout, "TOTPAL: %i\nPPG: %i\n", tot_pal, ppg);
-	for (proc = 0, word = 1; proc<num_procesos && word <= tot_pal; proc++){
-		fprintf(stdout, "Proc %i - [%i", proc, word);
-		if(word < tot_pal && word+ppg < tot_pal) {
-			fprintf(stdout, ", %i]\n", word+(ppg-1));
-			word+=ppg;
-		} else if(word < tot_pal && word+ppg == tot_pal && proc == num_procesos-1) {
-			fprintf(stdout, ", %i]\n", word+=ppg);
-		} else if(word < tot_pal && word+ppg >= tot_pal && proc == num_procesos-1) {
-			fprintf(stdout, ", %i]\n", tot_pal);
-			word=tot_pal+1;
-		} else {
-			fprintf(stdout, ", %i]\n", word+(ppg-1));
-			word+=ppg;
+static int print_words(const int f, const int l) {
+	int index;
+	int lon;
+	char* const word = calloc(longitud()+2, sizeof(char));
+
+	index = f;
+	while (index <= l){
+		if(genera_palabra(index, word) != OK) {
+			return !OK;
 		}
+		lon = strlen(word);
+		word[lon] = '\n';
+		word[lon + 1] = '\0';
+		if (write(1, word, strlen(word)) == -1){
+			perror("Whoops, el mensaje se perdió");
+		}
+		index++;
 	}
+	free(word);
 	return OK;
 }
 
-static int create_sons(const int ppg, const int num_procesos){
+static void print_pid(const int f, const int l){
+	char notif[45];
+	int thewr;
+
+	thewr = snprintf(notif, 45, "PID: %d - [%d, %d]\n", getpid(), f, l);
+
+	if(write(1, notif, thewr) == -1){
+		perror("Mensaje no impreso");
+	}
+}
+
+static void dealer(const int num_procesos) {
 	int word;
 	int proc;
 	int tot_pal;
-	tot_pal=total_palabras();
-	fprintf(stdout, "TOTPAL: %i\nPPG: %i\n", tot_pal, ppg);
-	for (proc = 0, word = 1; proc<num_procesos && word <= tot_pal; proc++){
-		fprintf(stdout, "Proc %i - [%i", proc, word);
-		if(word < tot_pal && word+ppg < tot_pal) {
-			fork();
-			word+=ppg;
-		} else if(word < tot_pal && word+ppg == tot_pal && proc == num_procesos-1) {
-			fprintf(stdout, ", %i]\n", word+=ppg);
-		} else if(word < tot_pal && word+ppg >= tot_pal && proc == num_procesos-1) {
-			fprintf(stdout, ", %i]\n", tot_pal);
-			word=tot_pal+1;
-		} else {
-			fprintf(stdout, ", %i]\n", word+(ppg-1));
-			word+=ppg;
+	int base_ppg;
+	int resto;
+	int start_word;
+	int end_word;
+	int words_for_proc;
+	int son;
+	pid_t pid = 1;
+
+	word = 0;
+	tot_pal = total_palabras();
+	base_ppg = tot_pal / num_procesos;
+	resto = tot_pal % num_procesos;
+
+	for (proc = 0; proc < num_procesos && word < tot_pal && pid != 0 ; proc++) {
+		start_word = word;
+		words_for_proc = base_ppg + (proc < resto ? 1 : 0);
+		end_word = start_word + words_for_proc - 1;
+		if (words_for_proc > 0) {
+			pid = fork();
+			if (pid == -1) {
+				perror("Proceso hijo no creado");
+			}
+
+			if(pid != 0){
+				word +=words_for_proc;
+			}
 		}
 	}
-	return OK;
+
+	if (pid != 0){
+		while (wait(&son) != -1);
+	} else {
+		print_words(start_word, end_word);
+	}
+
 }
+
+static void escribir_numword(const int num_procesos) {
+	int word;
+	int proc;
+	int tot_pal;
+	int base_ppg;
+	int resto;
+	int start_word;
+	int words_for_proc;
+
+	word = 0;
+	tot_pal = total_palabras();
+	base_ppg = tot_pal / num_procesos;
+	resto = tot_pal % num_procesos;
+
+	for (proc = 0; proc < num_procesos && word < tot_pal; proc++) {
+		start_word = word;
+		words_for_proc = base_ppg + (proc < resto ? 1 : 0);
+
+		if (words_for_proc > 0) {
+			fprintf(stdout, "Proc %i - [%i, %i]\n", proc, start_word, start_word 
+												+ words_for_proc - 1);
+			word += words_for_proc;
+		}
+	}
+}
+
+
+
 
 /* Realiza el trabajo del reventador una vez que se ha obtenido la
  * información necesaria de los argumentos del programa.
@@ -88,14 +146,9 @@ static int trabajar(const char alfabeto[], const int longitud, const int num_pro
 	{
 		return !OK;
 	}
-	if ((total_palabras()/num_procesos)%2==0 && (total_palabras()/num_procesos)*num_procesos!=num_procesos){
-		ppg = (total_palabras()/num_procesos) + 1;
-	} else {
-		ppg = (total_palabras()/num_procesos);
-	}
 	
-	return escribir_numword(ppg, num_procesos);
-	/*return escribir_palabras(); */
+	dealer(num_procesos);
+	return OK;
 }
 
 /**
@@ -144,7 +197,6 @@ int main(const int argc, const char * argv[])
 		fprintf(stderr, "los hijos no son válidos\n");
 		return !OK;
 	}
-	printf("Procesos a crear: %li\n", numero_procesos);
 
 	if (str2int(argv[2], &longitud) != OK)
 	{
