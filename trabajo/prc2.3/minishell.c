540f39f9e2805ebe19258560b0ff51b1ea6961a5
Jorge Benjumea <me@intron014.com>
2024-10-24 12:57:50 +0200

    p2.3 (Part 2)

--- trabajo/prc2.3/minishell.c
+++ trabajo/prc2.3/minishell.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
 #include "orden.h"
 
 #define BUFFER_SIZE 150
 
 enum errors{
 	SUCCESS,
+	ERR_MORE_ARGS_THAN_EXP,
+	ERR_NO_REDIRECT_ALLOWED,
+	ERR_NO_SALIDA_ALLOWED,
+	ERR_NO_ENTRADA_ALLOWED,
+	ERR_COMMAND_NO_RECOGNIZED,
 	ERROR
 };
 
@@ -29,54 +39,159 @@ void mostrar_orden(struct orden* o)
 	printf ("\n");
 }
 
-int exec_cd(struct orden* o){
-	if (o->args[2] != NULL){
-		fprintf(stderr, "%s\n", "More args than expected");
+int check_errors(struct orden* o, int c_args, int c_args_skip, int c_red_in, 
+					int c_red_out){
+	if (o->args[c_args] != NULL && !c_args_skip){
+		return ERR_MORE_ARGS_THAN_EXP;
 	}
-	chdir(o->args[1]);
+	if (c_red_in && c_red_out) {
+		if(o->entrada!=NULL || o->salida!=NULL){
+			return ERR_NO_REDIRECT_ALLOWED;
+		}
+	} else if (c_red_in) {
+		if(o->entrada != NULL){
+			return ERR_NO_ENTRADA_ALLOWED;
+		}
+	} else if (c_red_out) {
+		if (o-> salida != NULL) {
+			return ERR_NO_SALIDA_ALLOWED;
+		}
+	}
+	return SUCCESS;
+}
+
+int exec_cd(struct orden* o){
+	/*cd a b <-- error*/
+	/*cd*/
+	int resultado = check_errors(o, 2, 1, 1, 1); 
+	if(resultado!=SUCCESS){
+		return resultado;
+	}
+	if(o->args[1] == NULL){
+		char *path;
+		path = getenv("HOME");
+		if (path == NULL) {
+			fprintf(stderr, "%s", "La variable HOME no existe en este usuario");
+			return ERROR;
+		}
+		if(chdir(path) != 0){
+			perror("Error");
+			return ERROR;
+		}
+		return SUCCESS;
+	} else if(o->args[1] != NULL && o->args[2] == NULL){
+		if(chdir(o->args[1]) != 0){
+			perror("Error");
+			return ERROR;
+		}
+	} else {
+		return ERR_MORE_ARGS_THAN_EXP;
+	}
+	return SUCCESS;
 }
 
 int check_exit(struct orden* o){
-
+	int resultado = check_errors(o, 1, 0, 1, 1); 
+	return resultado;
 }
 
 int exec_pwd(struct orden* o){
 	char path[BUFFER_SIZE];
-	if(o->args[1]!=NULL){
-		fprintf(stderr, "%s\n", "More args than expected");
-		return ERROR;
+	/*Cosas not allowed*/
+	int resultado = check_errors(o, 1, 0, 1, 1);
+	if(resultado!=SUCCESS){
+		return resultado;
 	}
 
-	if(o->entrada!=NULL || o->salida!=NULL){
-		fprintf(stderr, "%s\n", "Este comando no acepta redirección");
-		return ERROR;
-	}
-
+	/*Errores de la función*/
 	if(getcwd(path, BUFFER_SIZE) == NULL) {
-		fprintf(stderr, "%s\n", "Something happened");
+		perror("Directorio actual no se pudo obtener");
 		return ERROR;
 	}
+
 	printf("%s\n", path);
 	return SUCCESS;
+}
+
+int exec_external_command (struct orden* actual){
+	int son;
+	pid_t pid = 1;
+
+	pid = fork();
+	if(pid == -1) {
+		perror("Proceso hijo no creado");
+	}
+
+	if(pid == 0) {
+		exit(execlp(actual->args[0], actual->args[1]));
+	}
+
+	if(pid != 0) {
+		int status;
+		pid_t finished_pid;
+		while ((finished_pid = waitpid(-1, &status, 0)) != -1) {            
+            if (WIFEXITED(status)) {
+                int exit_status = WEXITSTATUS(status);
+                if (exit_status == 0) {
+                    return SUCCESS;
+                } 
+            } 
+        }
+	}
+	return ERROR;
+		
+}
+
+int command_switcher(struct orden* actual){
+	if(strcmp("pwd", actual->args[0]) == 0){
+			return exec_pwd(actual);
+		} else if(strcmp("cd", actual->args[0]) == 0){
+			return exec_cd(actual);
+		} else if(strcmp("exit", actual->args[0])== 0){
+			return check_exit(actual);
+		} else {
+			/*return ERR_COMMAND_NO_RECOGNIZED; */
+			return exec_external_command(actual);
+		}
+}
+
+void show_error (enum errors error){
+	switch (error)
+	{
+		case ERR_MORE_ARGS_THAN_EXP:
+			fprintf(stderr, "%s\n", "More args than expected");
+			break;
+		case ERR_NO_REDIRECT_ALLOWED:
+			fprintf(stderr, "%s\n", "Este comando no acepta redirección");
+			break;
+		case ERR_NO_SALIDA_ALLOWED:
+			fprintf(stderr, "%s\n", "Este comando no acepta redirecciones de salida");
+			break;
+		case ERR_NO_ENTRADA_ALLOWED:
+			fprintf(stderr, "%s\n", "Este comando no acepta redirecciones de entrada");
+			break;
+		case ERR_COMMAND_NO_RECOGNIZED:
+			fprintf(stderr, "%s\n", "Comando no reconocido");
+			break;
+		default:
+			break;
+	}
 }
 
 int main(const int argc, const char* argv[])
 {
 	struct orden* actual;
+	int resultado;
 
 	do
 	{
 		actual = leer_orden("bt0354>");
-		if(strcmp("pwd", actual->args[0]) == 0){
-			exec_pwd(actual);
-		} else if(strcmp("cd", actual->args[0]) == 0){
-			exec_cd(actual);
-		} else if(strcmp("exit", actual->args[0])== 0){
-			check_exit(actual);
-		} else {
-			fprintf(stderr, "%s\n", "Comando no reconocido");
+		resultado = command_switcher(actual);
+		if(resultado != SUCCESS) {
+			show_error(resultado);
 		}
 		
-	} while (strcmp("exit", actual->args[0]) != 0);
+	} while ((strcmp("exit", actual->args[0]) != 0) || (resultado != SUCCESS));
+	printf("\n");
 	return 0;
 }
